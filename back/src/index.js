0f45a9db43ff8090a27c0d810c4c661882aa34dd
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-15 15:47:42 +0100

    feat: add all call router

--- back/src/index.js
+++ back/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const smurfsRouters = require('./routers/smurfs_routers');
+const smurfRouters = require('./routers/smurf_routers');
 require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use('/api/smurfs', smurfsRouters);
+app.use('/api/smurf', smurfRouters);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${ PORT }`);
